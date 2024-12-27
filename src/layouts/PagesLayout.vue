85d6d75176f59c541832a07539ac946b9bda2368
osahene <bra.kwameadu3@gmail.com>
2024-12-27 11:10:19 +0000

    mob

--- src/layouts/PagesLayout.vue
+++ src/layouts/PagesLayout.vue
@@ -1,18 +1,22 @@
 <template>
-  <q-layout view="hHh LpR fFf">
-    <q-page-container class="bg-accent q-pa-sm w-full min-w-full">
-      <router-view />
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="bg-accent">
+      <div class="relative">
+        <div class="px-4 md:px-10 mx-auto w-full -m-24">
+          <router-view />
+          <q-footer>
+            <q-tabs v-model="tab" align="justify" dense class="bg-teal text-yellow shadow-2">
+              <q-route-tab to="/pages/home" name="home" icon="home" class="q-pa-sm" />
+              <q-route-tab to="/pages/edit" name="edit" icon="edit" />
+              <q-route-tab to="/pages/list" name="lists" icon="list">
+                <q-badge color="purple" text-color="white" floating>5+</q-badge>
+              </q-route-tab>
+              <q-route-tab name="more" icon="more" />
+            </q-tabs>
+          </q-footer>
+        </div>
+      </div>
     </q-page-container>
-    <q-footer>
-      <q-tabs v-model="tab" align="justify" dense class="bg-teal text-yellow shadow-2">
-        <q-route-tab to="/pages/home" name="home" icon="home" class="q-pa-sm" />
-        <q-route-tab to="/pages/edit" name="edit" icon="edit" />
-        <q-route-tab to="/pages/list" name="lists" icon="list">
-          <q-badge color="purple" text-color="white" floating>5+</q-badge>
-        </q-route-tab>
-        <q-route-tab name="more" icon="more" />
-      </q-tabs>
-    </q-footer>
   </q-layout>
 </template>
 
