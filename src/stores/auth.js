0d928c145b666a06526846cafb7fbb029b1fe61d
osahene <bra.kwameadu3@gmail.com>
2025-04-12 07:24:36 +0000

    used access_token

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -46,7 +46,7 @@ export const useAuthStore = defineStore('auth', {
           redirectUri: window.location.origin // This shows what's being sent
         })
         const res = await apiService.googleLogin({
-          id_token: response.id_token,
+          id_token: response.access_token,
         })
         if (res.status === 200) {
           const userDetails = {
