6d7f27cfa679d04c9d7b6db385d76db1e4b728c6
osahene <bra.kwameadu3@gmail.com>
2025-04-13 15:07:11 +0000

    push back

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,62 +1,53 @@
-// src/boot/axios.js
-import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 import { jwtDecode } from 'jwt-decode'
 import dayjs from 'dayjs'
 import { Loading, QSpinnerBall, Notify } from 'quasar'
 
-let isRefreshing = false
-let failedQueue = []
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error)
-    } else {
-      prom.resolve(token)
-    }
-  })
-  failedQueue = []
-}
-
+// Creating Axios instance with default configuration
 const $axios = axios.create({
-  baseURL: process.env.VITE_baseURL || 'http://127.0.0.1:8000',
+  baseURL: process.env.VITE_baseURL ? process.env.VITE_baseURL : 'http://127.0.0.1:8000',
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  'Content-type': 'application/json',
 })
 
-const refreshToken = async () => {
-  const refresh = localStorage.getItem('refreshToken')
-  // if (!refresh) return null
-  console.log('Refreshing token not found', refresh)
+// Function to refresh access token
+const TakeRefreshToken = async () => {
+  const refreshToken = localStorage.getItem('refreshToken')
 
+  if (!refreshToken) return null
   try {
     const response = await axios.post(`${$axios.defaults.baseURL}/account/token/refresh/`, {
-      refresh,
+      refresh: refreshToken,
     })
-    console.log('Response from refresh token:', response)
-    const { access, refresh: newRefresh } = response.data
-
+    console.log('Response from refresh token:', response.data)
+    const { access, refresh } = response.data
     if (access) {
-      localStorage.setItem('accessToken', access)
       $axios.defaults.headers.common['Authorization'] = `Bearer ${access}`
+      localStorage.setItem('accessToken', access)
+      // Update refresh token if a new one is provided
+      if (refresh) {
+        localStorage.setItem('refreshToken', refresh)
+      }
+      return { accessToken: access, refreshToken: refresh || refreshToken }
+    }
+  } catch (e) {
+    let errorMessage = 'Error refreshing token'
+
+    if (e.response && e.response.data && e.response.data.detail) {
+      errorMessage = e.response.data.detail
+    } else if (e.message) {
+      errorMessage = e.message
     }
 
-    if (newRefresh) {
-      localStorage.setItem('refreshToken', newRefresh)
-    }
-
-    return access
-  } catch (error) {
-    const errorMessage = error?.response?.data?.detail || error?.message || 'Error refreshing token'
-    Notify.create({ type: 'negative', message: errorMessage })
+    Notify.create({
+      type: 'negative',
+      message: errorMessage,
+    })
     return null
   }
 }
 
-export default boot(({ app, router }) => {
+export default async ({ router }) => {
   let userIsActive = true
 
   const setUserActive = () => {
@@ -64,107 +55,90 @@ export default boot(({ app, router }) => {
     localStorage.setItem('lastActive', new Date().toISOString())
   }
 
+  // Event listeners to track user activity
   window.addEventListener('mousemove', setUserActive)
   window.addEventListener('keydown', setUserActive)
   window.addEventListener('scroll', setUserActive)
 
-  setInterval(async () => {
-    const accessToken = localStorage.getItem('accessToken')
-    const refreshTokenValue = localStorage.getItem('refreshToken')
-    console.log('Access Token: in interval', accessToken)
-    if (!accessToken && !refreshTokenValue) return
-    console.log('Access Token: after interval', accessToken)
+  // Function to schedule token refresh checks
+  const scheduleTokenRefresh = () => {
+    setInterval(async () => {
+      const accessTokens = localStorage.getItem('accessToken')
+      const refreshTokens = localStorage.getItem('refreshToken')
 
-    const now = dayjs()
-    const accessExp = accessToken ? dayjs.unix(jwtDecode(accessToken).exp) : null
-    console.log('Access Exp:', accessExp)
-    const refreshExp = refreshTokenValue ? dayjs.unix(jwtDecode(refreshTokenValue).exp) : null
-    const lastActive = dayjs(localStorage.getItem('lastActive'))
+      if (accessTokens && refreshTokens) {
+        const user = jwtDecode(accessTokens)
+        const accessExpTime = dayjs.unix(user.exp)
+        const now = dayjs()
 
-    const accessAboutToExpire = accessExp && accessExp.diff(now, 'second') < 60
-    const refreshAboutToExpire = refreshExp && refreshExp.diff(now, 'second') < 60
-    const userInactive = now.diff(lastActive, 'minute') >= 10
+        const refreshTokenExpTime = dayjs.unix(jwtDecode(refreshTokens).exp)
 
-    if ((accessAboutToExpire || refreshAboutToExpire) && userIsActive) {
-      console.log('calling for token')
-      await refreshToken()
-    }
-
-    if (refreshAboutToExpire && userInactive) {
-      console.log('User inactive and refresh token expired')
-      localStorage.removeItem('accessToken')
-      localStorage.removeItem('refreshToken')
-      userIsActive = false
-      router.push('/auth/login')
-    }
-  }, 30000)
-
-  $axios.interceptors.request.use(
-    async (config) => {
-      Loading.show({
-        spinner: QSpinnerBall,
-        message: 'Loading...',
-        spinnerSize: 140,
-        spinnerColor: 'red',
-        sanitize: true,
-      })
-
-      let accessToken = localStorage.getItem('accessToken')
-      console.log('Access Token making request:', accessToken)
-      if (accessToken) {
-        console.log('Access Token:', accessToken)
-        const decoded = jwtDecode(accessToken)
-        console.log('Decoded Token:', decoded)
-        const isExpired = dayjs.unix(decoded.exp).isBefore(dayjs())
-
-        if (isExpired) {
-          if (!isRefreshing) {
-            isRefreshing = true
-            try {
-              const newToken = await refreshToken()
-              accessToken = newToken
-              processQueue(null, newToken)
-            } catch (err) {
-              processQueue(err, null)
-              throw err
-            } finally {
-              isRefreshing = false
-            }
-          }
-
-          return new Promise((resolve, reject) => {
-            failedQueue.push({
-              resolve: (token) => {
-                config.headers['Authorization'] = `Bearer ${token}`
-                resolve(config)
-              },
-              reject: (err) => {
-                reject(err)
-              },
-            })
-          })
+        // Refresh the access token if it will expire in the next minute and the user is active
+        if (accessExpTime.diff(now, 'minute') <= 1 && userIsActive) {
+          await TakeRefreshToken()
         }
 
-        config.headers['Authorization'] = `Bearer ${accessToken}`
-      }
+        // Refresh the refresh token if it will expire in the next minute and the user is active
+        if (refreshTokenExpTime.diff(now, 'minute') <= 1 && userIsActive) {
+          await TakeRefreshToken()
+        }
 
-      return config
-    },
-    (error) => Promise.reject(error),
-  )
+        // Log out user if the refresh token will expire in the next minute and they are inactive
+        const lastActive = dayjs(localStorage.getItem('lastActive'))
+        if (now.diff(lastActive, 'minute') >= 10 && refreshTokenExpTime.diff(now, 'minute') <= 1) {
+          localStorage.removeItem('accessToken')
+          localStorage.removeItem('refreshToken')
+          userIsActive = false
+          router.push('/auth/login') // Or your login route
+        }
+      }
+    }, 30000) // Check every minute
+  }
+
+  scheduleTokenRefresh()
+
+  $axios.interceptors.request.use(async (req) => {
+    let accessToken = localStorage.getItem('accessToken')
+    Loading.show({
+      spinner: QSpinnerBall,
+      message: 'Loading...',
+      spinnerSize: 140,
+      spinnerColor: 'red',
+      sanitize: true,
+    })
+
+    if (accessToken) {
+      const user = jwtDecode(accessToken)
+      const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+
+      if (!isExpired) {
+        req.headers.Authorization = `Bearer ${accessToken}`
+      } else {
+        const tokens = await TakeRefreshToken()
+        if (tokens && tokens.accessToken) {
+          req.headers.Authorization = `Bearer ${tokens.accessToken}`
+        } else {
+          localStorage.removeItem('accessToken')
+          localStorage.removeItem('refreshToken')
+          router.push('/auth/login')
+        }
+      }
+    }
+    return req
+  })
 
   $axios.interceptors.response.use(
     (response) => {
+      // Hide loading spinner after response
       Loading.hide()
       return response
     },
     (error) => {
+      // Hide loading spinner on error
       Loading.hide()
       return Promise.reject(error)
     },
   )
-
-  app.config.globalProperties.$axios = $axios
-})
+}
 
 export { $axios }
