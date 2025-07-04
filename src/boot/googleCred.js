5bce8bbf1815c5e77887e817725879e42e39ba4e
osahene <bra.kwameadu3@gmail.com>
2025-07-04 09:30:52 +0000

    feat: enable Google Sign-In plugin and clean up initialization code

--- src/boot/googleCred.js
+++ src/boot/googleCred.js
@@ -1,44 +1,44 @@
 import { boot } from 'quasar/wrappers'
-// import GoogleSignInPlugin from 'vue3-google-signin'
+import GoogleSignInPlugin from 'vue3-google-signin'
 import { SocialLogin } from '@capgo/capacitor-social-login'
-// import { useQuasar } from 'quasar'
+import { Capacitor } from '@capacitor/core'
 
-// const $q = useQuasar()
+const isNativeMobile = Capacitor.isNativePlatform()
 
 export default boot(({ app }) => async () => {
-  app.use(
-    await SocialLogin.initialize({
-      google: {
-        webClientId: '972387283638-50fgpr3klnhg5ld92jtam1iu69n4s7gf.apps.googleusercontent.com',
+  // app.use(
+  //   await SocialLogin.initialize({
+  //     google: {
+  //       webClientId: '972387283638-50fgpr3klnhg5ld92jtam1iu69n4s7gf.apps.googleusercontent.com',
+  //     },
+  //   }),
+  // )
+  if (isNativeMobile) {
+    app.use(
+      SocialLogin.initialize({
+        google: {
+          androidClientId:
+            '972387283638-50fgpr3klnhg5ld92jtam1iu69n4s7gf.apps.googleusercontent.com',
+        },
+      }),
+    )
+  } else {
+    app.use(GoogleSignInPlugin, {
+      clientId:
+        process.env.NODE_ENV === 'development'
+          ? process.env.VITE_googleCred
+          : process.env.VITE_clientId,
+      scope: 'profile email',
+      response_type: 'id_token',
+      prompt: 'consent',
+      redirect_uri: 'https://helpmobile.vercel.app',
+      onSuccess: (response) => {
+        // Optional: Handle successful login globally (e.g., redirect)
+        console.log('Google Login Success:', response)
       },
-    }),
-  )
-  // if ($q.platform.is.mobile) {
-  //   app.use(
-  //     SocialLogin.initialize({
-  //       google: {
-  //         androidClientId:
-  //           '972387283638-50fgpr3klnhg5ld92jtam1iu69n4s7gf.apps.googleusercontent.com',
-  //       },
-  //     }),
-  //   )
-  // } else {
-  //   app.use(GoogleSignInPlugin, {
-  //     clientId:
-  //       process.env.NODE_ENV === 'development'
-  //         ? process.env.VITE_googleCred
-  //         : process.env.VITE_clientId,
-  //     scope: 'profile email',
-  //     response_type: 'id_token',
-  //     prompt: 'consent',
-  //     redirect_uri: 'https://helpmobile.vercel.app',
-  //     onSuccess: (response) => {
-  //       // Optional: Handle successful login globally (e.g., redirect)
-  //       console.log('Google Login Success:', response)
-  //     },
-  //     onFailure: (error) => {
-  //       console.error('Google Login Error:', error)
-  //     },
-  //   })
-  // }
+      onFailure: (error) => {
+        console.error('Google Login Error:', error)
+      },
+    })
+  }
 })
